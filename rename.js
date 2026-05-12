fa3136c5769465244d1d708e503d2f8963cd3a27
Richa Singh <richa.singhnxt213@gmail.com>
2026-05-12 16:40:22 +0530

    moving and deleting a file

--- rename.js
+++ rename.js
@@ -1,2 +1,20 @@
 const fs = require("fs");
-fs.renameSync("./assets/color.json", "./assets/color-data.json");
+//rename file
+// fs.renameSync("./assets/color.json", "./assets/color-data.json");
+
+//moving file from one folder to desired one
+// fs.rename("./assets/notes.md", "./storage-files/notes.md", (err) => {
+//   if (err) {
+//     console.log(err.message);
+//   }
+// });
+
+// moving file to anither folder with different file name
+// fs.rename("./assets/notes.md", "./storage-files/note.md", (err) => {
+//   if (err) {
+//     console.log(err.message);
+//   }
+// });
+
+// deleting an existing file.
+fs.unlinkSync("./assets/async-notes.md");
