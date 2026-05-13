8a9a62d2e051837c63d639af84d96bee5a7cc047
Richa Singh <richa.singhnxt213@gmail.com>
2026-05-13 18:20:22 +0530

    reading text chunk by chunk form one file and write chunk by chunk in another

--- writestream.js
+++ writestream.js
@@ -1,5 +1,11 @@
 //writing text using stream writer (byte by byte)
 const fs = require("fs");
 const writestream = fs.createWriteStream("./assets/stream-created-text.md");
-writestream.write("hi");
-writestream.write("richa");
+// writestream.write("hi");
+// writestream.write("richa");
+
+// reading text chunk by chunk form one file and write chunk by chunk in another
+const readStream = fs.createReadStream("./assets/Readme.md");
+readStream.on("data", (data) => {
+  writestream.write(data);
+});
