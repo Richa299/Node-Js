de15b1e8f0fe70af38124b8d2ddefeb614b53bf0
Richa Singh <richa.singhnxt213@gmail.com>
2026-05-13 15:32:21 +0530

    deleting the directory when it has some content/files inside

--- directories.js
+++ directories.js
@@ -3,5 +3,16 @@ const fs = require("fs");
 // fs.renameSync("./dummy-folder", "./dummy-data-folder");
 
 // deleting the folder/directory
-fs.rmdirSync("./dummy-data-folder");
+// fs.rmdirSync("./dummy-data-folder");
 // if the folder/directory consist of some contents inside it or some file the will get error.
+
+//This is when we have file inside the folder which we want to delete.. so will read the directory/folder and line-by-line delete the file
+fs.readdirSync("./dummy-data-folder").forEach((file) => {
+  fs.unlinkSync(`./dummy-data-folder/${file}`);
+});
+
+fs.rmdir("./dummy-data-folder", (err) => {
+  if (err) {
+    console.log(err.message);
+  }
+});
